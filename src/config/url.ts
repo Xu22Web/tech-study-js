79419a16137e673be7f922f49523e968839f5a49
Noah <1627295329@qq.com>
2023-01-07 04:08:07 +0800

    chore: update url config

--- src/config/url.ts
+++ src/config/url.ts
@@ -2,8 +2,10 @@
  * @description url配置
  */
 const URL_CONFIG = {
-  // 主页
+  // 主页正则
   home: /^https\:\/\/www\.xuexi\.cn(\/(index\.html)?)?$/,
+  // 主页
+  homeOrigin: 'https://www.xuexi.cn',
   // 每日答题页面
   examPractice: 'https://pc.xuexi.cn/points/exam-practice.html',
   // 专项练习页面

529afc8d8dc7626983dfdaaefeb1b9d88a120814
Noah <1627295329@qq.com>
2023-01-07 04:08:37 +0800

    chore: add some utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -417,6 +417,314 @@ function generateMix(length: number = 6): string {
   }
   return randomText.join('');
 }
+/**
+ * @description 格式化日期时间数字
+ * @param num
+ * @returns
+ */
+function formatDateNum(num: number) {
+  return num < 10 ? `0${num}` : `${num}`;
+}
+
+/**
+ * @description 格式化日期时间
+ * @param time
+ * @returns
+ * @example
+ * formatDateTime() -> "2022-09-01 08:00:00"
+ * formatDateTime(new Date()) -> "2022-09-01 08:00:00"
+ * formatDateTime(Date.now()) -> "2022-09-01 08:00:00"
+ */
+function formatDateTime(time: Date | string | number = Date.now()) {
+  const date = new Date(time);
+  const s = date.getSeconds();
+  const min = date.getMinutes();
+  const h = date.getHours();
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  // 日期
+  const dateText = [y, m, d].map(formatDateNum).join('-');
+  // 时间
+  const timeText = [h, min, s].map(formatDateNum).join(':');
+  // 日期时间
+  const dateTimeText = `${dateText} ${timeText}`;
+  return dateTimeText;
+}
+/**
+ * @description 格式化时间
+ * @param time
+ * @returns
+ * @example
+ * formatTime() -> "08:00:00"
+ * formatTime(new Date()) -> "08:00:00"
+ * formatTime(Date.now()) -> "08:00:00"
+ */
+const formatTime = (time: Date | string | number = Date.now()) => {
+  const date = new Date(time);
+  const s = date.getSeconds();
+  const min = date.getMinutes();
+  const h = date.getHours();
+  // 时间
+  const timeText = [h, min, s].map(formatDateNum).join(':');
+  return timeText;
+};
+/**
+ * @description html进度条
+ * @param title
+ * @param percent
+ * @returns
+ */
+function getProgressHTML(title: string, percent: number) {
+  // html
+  const progressHTML = `<div
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 1px 0;
+    "
+  >
+    <span>${title}</span>
+    <span>${getHighlightHTML(percent)} %</span>
+  </div>
+  <div
+    style="
+      background: white;
+      border-radius: 10px;
+      height: 10px;
+      border: 1px solid #eee;
+      flex-shrink: 1;
+    "
+  >
+    <div
+      style="
+        background: linear-gradient(to left, #188fff80, #1890ff);
+        height: 100%;
+        width: ${percent}%;
+        border-radius: 10px;
+      "
+    ></div>
+  </div>`;
+  return progressHTML;
+}
+/**
+ * @description html高亮文本
+ * @param text
+ * @returns
+ */
+function getHighlightHTML(text: string | number) {
+  // html
+  const highlightHTML = `<span style="color: #1890ff">${text}</span>`;
+  return highlightHTML;
+}
+/**
+ * @description 二维码
+ * @param src
+ */
+function getImgHTML(src: string) {
+  // 图片
+  return `
+     <div style="padding: 10px 0">
+     <div
+       style="
+         display: flex;
+         justify-content: center;
+         align-items: center;
+         padding: 20px;
+         background: #f7f7f7;
+         border-radius: 10px;
+       "
+     >
+         <img src="${src}" style="" />
+       </div>
+     </div>
+`;
+}
+/**
+ * @description 创建模态框
+ * @param options 选项
+ * @returns
+ */
+function createModal(options: ModalOptions) {
+  // 配置
+  const {
+    title,
+    subTitle = '',
+    to = '用户',
+    content,
+    type,
+    from = 'tech-study.js',
+  } = options;
+  // 内容文本
+  let contentText = '';
+  if (Array.isArray(content)) {
+    contentText = content.map((ct) => `<div>${ct}</div>`).join('');
+  } else {
+    contentText = content;
+  }
+  // 日期
+  const dateTime = formatDateTime();
+  // 类型html
+  let typeHTML = '';
+  if (type && type.length) {
+    if (type === 'info') {
+      typeHTML = `
+      <svg
+       viewBox="64 64 896 896"
+       style="color: #1890ff; width: 18px; height: 18px"
+       fill="currentColor"
+       aria-hidden="true"
+     >
+       <path
+         d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"
+       ></path>
+     </svg>`;
+    }
+    if (type === 'warn') {
+      typeHTML = `
+      <svg
+        viewBox="64 64 896 896"
+        style="color: #faad14; width: 18px; height: 18px"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm-32 232c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v272c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V296zm32 440a48.01 48.01 0 010-96 48.01 48.01 0 010 96z"
+        ></path>
+      </svg>
+      `;
+    }
+    if (type === 'success') {
+      typeHTML = `
+      <svg
+        viewBox="64 64 896 896"
+        style="color: #52c41a; width: 18px; height: 18px"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z"
+        ></path>
+      </svg>
+      `;
+    }
+    if (type === 'fail') {
+      typeHTML = `
+      <svg
+        viewBox="64 64 896 896"
+        style="color: #ff4d4f; width: 18px; height: 18px"
+        fill="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4-66.1.3c-4.4 0-8-3.5-8-8 0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 01-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4 66-.3c4.4 0 8 3.5 8 8 0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2 0 4.4-3.6 8-8 8z"
+        ></path>
+      </svg>
+      `;
+    }
+  }
+  // 类型
+  const typeWrap = `
+  <span
+    style="
+      padding-right: 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    ${typeHTML}
+  </span>
+  `;
+  // 基础html
+  const baseHTML = `
+  <div
+  style="
+    padding: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  "
+>
+  <div
+    style="
+      background: #ffffff;
+      box-shadow: 1px 1px 8px -1px #dadada;
+      padding: 5px 10px;
+      border-radius: 5px;
+      width: 100%;
+    "
+  >
+    <div
+      style="
+        display: flex;
+        justify-content: space-between;
+        padding: 5px;
+        border-bottom: 1px solid #eee;
+      "
+    >
+      <div style="display: flex; justify-content: center; align-items: center">
+        ${typeWrap}
+        <span style="padding-left: 5px; font-size: 18px">${title}</span>
+      </div>
+      <div style="font-size: 16px; color: #999">${subTitle}</div>
+    </div>
+    <div></div>
+
+    <div style="padding:10px 5px; font-size: 16px; min-height: 80px">
+      <div>
+        ${getHighlightHTML(to)}, 你好!
+      </div>
+      <div style="line-height: 28px;">${contentText}</div>
+    </div>
+    <div
+      style="
+        font-size: 14px;
+        padding: 5px;
+        border-top: 1px solid #eee;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      "
+    >
+      <div style="color: #999">${dateTime}</div>
+      <div>
+        <span>来自</span>
+        <span style="color: #1890ff; padding-left: 1px">${from}</span>
+      </div>
+    </div>
+  </div>
+</div>  
+  `;
+  return baseHTML;
+}
+/**
+ * @description 时间已过
+ * @param hour
+ * @param minute
+ * @returns
+ */
+function isLate({ hour, minute }: { hour: number; minute: number }) {
+  const date = new Date();
+  const h = date.getHours();
+  const min = date.getMinutes();
+  return h > hour || (h === hour && min >= minute);
+}
+
+/**
+ * @description 时间已过
+ * @param hour
+ * @param minute
+ * @returns
+ */
+function isNow({ hour, minute }: { hour: number; minute: number }) {
+  const date = new Date();
+  const h = date.getHours();
+  const min = date.getMinutes();
+  const s = date.getSeconds();
+  return h === hour && min === minute && s === 0;
+}
 export {
   $$,
   openWin,
@@ -432,4 +740,12 @@ export {
   createRandomPoint,
   createRandomPath,
   generateMix,
+  createModal,
+  getImgHTML,
+  formatTime,
+  formatDateNum,
+  isNow,
+  isLate,
+  getProgressHTML,
+  getHighlightHTML,
 };
